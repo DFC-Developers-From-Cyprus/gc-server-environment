bf9669ec8727af419d3e44bd83d25ab1c0da2ec0
StudentFPW <jofeleonids00@gmail.com>
2025-04-29 22:09:11 +0300

    added get by uuid is getting location

--- environment/pollution_measurement/viewsets.py
+++ environment/pollution_measurement/viewsets.py
@@ -1,11 +1,14 @@
 import uuid
 
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from .models import PollutionMeasurement
 from .serializers import PollutionMeasurementSerializer
+from polluted_area.models import PollutedArea
 
+from .requests import test
 from .extend_schema import (
     parameters_schema_decorator,
     get_by_id_schema_decorator,
@@ -18,4 +21,8 @@ class PollutionMeasurementViewSet(viewsets.ModelViewSet):
 
     @get_by_id_schema_decorator
     def retrieve(self, request, *args, **kwargs):
-        pass
+        polluted_area_uuid = kwargs.get("uuid")
+        polluted_area_object = get_object_or_404(PollutedArea, uuid=polluted_area_uuid)
+        location = polluted_area_object.location
+        print(test(location))
+        return Response(200)
