95424c5c6bddd1557be05c74b7abee85876a4f90
StudentFPW <jofeleonids00@gmail.com>
2025-04-29 22:11:21 +0300

    added comment

--- environment/pollution_measurement/requests.py
+++ environment/pollution_measurement/requests.py
@@ -10,3 +10,7 @@ TOKEN = os.getenv("TOKEN")
 def test(location):
     test_link = f"https://api.waqi.info/feed/{location}/?token={TOKEN}"
     return requests.get(test_link).content
+
+
+# https://docs.openaq.org/about/about
+# https://aqicn.org/api/ru/
